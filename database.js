23803442210c10fca16eeff1a98a21c536f0a56c
JorgeM8rales <69659467+JorgeM8rales@users.noreply.github.com>
2022-03-24 18:47:02 -0600

    actualizacion

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 let config = {
 	host: "us-cdbr-east-05.cleardb.net",
-	user: "rb185e8ef61e7ca",
+	user: "b185e8ef61e7ca",
 	password: "c8cec538",
 	port: 3306,
 	database: "heroku_9de0f1f870b8662",
